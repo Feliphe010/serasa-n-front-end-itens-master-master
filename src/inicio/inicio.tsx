78c859ed7dfbb0b4ab766e1de5f97b95a62827b3
Cintia <cintiapedroso2005@gmail.com>
2022-06-21 20:00:51 -0300

    Enter

--- src/inicio/inicio.tsx
+++ src/inicio/inicio.tsx
@@ -35,10 +35,10 @@ const Pag = function(){
             <Navbar.Brand href="#Inicio">
 
               <img
-          className="d-20 w-80"
-         src="https://cdn.dribbble.com/users/2468490/screenshots/5579451/pet_logo.jpg" height="41" width="50"
+                className="d-20 w-80"
+                src="https://cdn.dribbble.com/users/2468490/screenshots/5579451/pet_logo.jpg" height="41" width="50"
        
-          />
+               />
 
 
             </Navbar.Brand>
@@ -48,33 +48,32 @@ const Pag = function(){
 
 
           
-                  <Nav.Link href="#Inicio" >
+                  <Nav.Link href="#Facebook" >
 
-            <img  
-                className="d-20 w-80"
-                src="https://confradesp.com.br/wp-content/uploads/2019/01/logo-face.png" height="25" width="30"
+                <img  
+                  className="d-20 w-80"
+                  src="https://confradesp.com.br/wp-content/uploads/2019/01/logo-face.png" height="25" width="30"
               
-            />
+                />
 
                   </Nav.Link>
              
 
-                  <Nav.Link href="#Adoções">
+                  <Nav.Link href="#Instagram">
                   
-                  <img  
-                className="d-20 w-80"
-                src="https://imagepng.org/wp-content/uploads/2017/08/instagram-icone-icon-1.png" height="25" width="30"
-              
-            />
+                <img  
+                  className="d-20 w-80"
+                  src="https://imagepng.org/wp-content/uploads/2017/08/instagram-icone-icon-1.png" height="25" width="30"
+                />
+
                   </Nav.Link>
                   
-                  <Nav.Link href="#Doações">
+                  <Nav.Link href="#Twitter">
                   
-                  <img  
-                className="d-20 w-80"
-                src="https://imagepng.org/wp-content/uploads/2018/08/twitter-icone-1.png" height="25" width="30"
-              
-            />
+                <img  
+                  className="d-20 w-80"
+                  src="https://imagepng.org/wp-content/uploads/2018/08/twitter-icone-1.png" height="25" width="30"
+                />
                   
                   </Nav.Link>
 
